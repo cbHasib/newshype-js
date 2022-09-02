9666f14cf56983817ad5b9674b555a6f50def8ab
cbHasib <hasibul.pbn@gmail.com>
2022-09-03 01:42:51 +0600

    Category data recieve

--- js/app.js
+++ js/app.js
@@ -5,9 +5,35 @@ const loadData = async (url) => {
     // console.log(data.data.news_category[0].category_name);
     return data;
   } catch (error) {
-    console.log(error);
+    const errorElement = document.getElementById('error-element');
+    errorElement.classList.remove('hidden');
+    const errorMessage = document.getElementById('error-message');
+    errorMessage.innerText = error;
   }
 };
+
+
+
+const showCategoryContent = async(categoryId) => {
+  const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`;
+  const newsData = await loadData(url);
+  const newsAll = newsData.data;
+
+  const newsContainer = document.getElementById('news-container');
+
+  newsAll.forEach(news => {
+    const {title, total_view, rating: {number, badge}, author: {name, published_date, img}, thumbnail_url, image_url, details, others_info: {is_todays_pick, is_trending}} = news;
+
+    
+
+
+
+
+
+  });
+}
+
+
 
 const getCategory = async () => {
   const categoryContainer = document.getElementById("category-container");
@@ -18,11 +44,15 @@ const getCategory = async () => {
   const categories = categoryData.data.news_category;
 
   for (const category of categories) {
-    const categoryName = (category.category_name);
+    const {category_id, category_name} = category;
     const li = document.createElement('li');
-    li.innerText = categoryName;
+    li.innerText = category_name;
+    li.setAttribute('onclick', `showCategoryContent('${category_id}')`)
     categoryContainer.appendChild(li);
   }
 };
 
 getCategory();
+
+
+
