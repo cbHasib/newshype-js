96e0f17d99be83b60366d86e0b93149b193d360f
cbHasib <hasibul.pbn@gmail.com>
2022-09-03 13:24:22 +0600

    Update Post modal info

--- js/post.js
+++ js/post.js
@@ -3,7 +3,7 @@ const blogPost = [
     {
         postId: 01,
         postTitle: 'Difference between var, let and const in JavaScript',
-        postDetails: 'The scope of a <code>var</code> variable is functional scope. The scope of a let and const variable is block scope. Var and let can be declared without initialization but const cannot e   declared without initialization. Var and let value can be reassign but const value cannot be reassign.',
+        postDetails: 'The scope of a <code>var</code> variable is functional scope. The scope of a <code>let</code> and <code>const</code> variable is block scope. <code>Var</code> and <code>let</code> can be declared without initialization but <code>const</code> cannot be declared without initialization. <code>Var</code> and <code>let</code> value can be reassign but <code>const</code> value cannot be reassign.',
         postThumb: 'https://miro.medium.com/max/800/1*SGbosJ1KwQTCFY326o7_Mw.jpeg'
     },
 
@@ -20,7 +20,7 @@ const blogPost = [
     {
         postId: 03,
         postTitle: '.forEach(), .map(), .filter(), .find()... What is the difference?',
-        postDetails: '.forEach(), is used to execute the same code on every element in an array but does not change the array and it returns undefined. .map() executes the same code on every element in an array and returns a new array with the updated elements. .filter() checks every element in an array to see if it meets a certain criteria and returns a new array with the elements that return truthy for the criteria. .find() checks every element in an array to find a specific element and the value of the first element that passes a test.',
+        postDetails: '<code>.forEach()</code>, is used to execute the same code on every element in an array but does not change the array and it returns undefined. <br><br> <code>.map()</code> executes the same code on every element in an array and returns a new array with the updated elements. <br><br><code>.filter()</code> checks every element in an array to see if it meets a certain criteria and returns a new array with the elements that return truthy for the criteria. .find() checks every element in an array to find a specific element and the value of the first element that passes a test.',
         postThumb: 'https://csharpcorner-mindcrackerinc.netdna-ssl.com/article/simplify-map-reduce-and-filter-in-typescript/Images/map_filter_reduce.png'
     },
 
@@ -30,7 +30,7 @@ const blogPost = [
     {
         postId: 04,
         postTitle: 'Why would you use template string?',
-        postDetails: 'Template strings are a powerful feature of modern JavaScript released in ES6. It lets us insert/interpolate variables and expressions into strings without needing to concatenate like in older versions of JavaScript. It allows us to create strings that are complex and contain dynamic elements. Another great thing that comes with template strings are tags. Tags are functions that take a string and the decomposed parts of the string as parameters and are great for converting strings to different entities.',
+        postDetails: 'Template strings are a powerful feature of modern JavaScript released in <code>ES6</code>. It lets us insert/interpolate <code>variables</code> and <code>expressions</code> into strings without needing to concatenate like in older versions of JavaScript. It allows us to create strings that are complex and contain dynamic elements. Another great thing that comes with template strings are tags. Tags are functions that take a string and the decomposed parts of the string as parameters and are great for converting strings to different entities.',
         postThumb: 'https://www.stanleyulili.com/assets/images/posts/2019-10-29-template-literals/featured-image.jpg'
     }
 
